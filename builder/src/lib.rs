47e03e7f191c152f8d093634e572de2bca34be69
Hyunggyu Jang <murasakipurplez5@gmail.com>
2022-12-03 17:18:25 +0900

    Debug using extra-traits

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,14 +1,50 @@
 use proc_macro::TokenStream;
-use quote::{quote, format_ident, ToTokens};
+use quote::{quote, format_ident, ToTokens, __private::TokenTree};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let structure = syn::parse::<syn::ItemStruct>(input).unwrap();
     let fields = structure.fields;
-    let ty: Vec<_> = fields.iter().map(|field| field.ty.to_token_stream()).collect();
-    println!("Ty {:#?}", ty);
     let ident = structure.ident;
     let builder = format_ident!("{}Builder", ident);
+    let fields_with_optional = fields
+        .iter()
+        .map(|field| {
+            println!("Ty {:?}", field.ty);
+            let optional = if let Some(TokenTree::Ident(id)) = field.ty.to_token_stream().into_iter().next() {
+                id.to_string() == "Option"
+            } else {
+                false
+            };
+            (field, optional)
+        });
+    let builder_fields = fields_with_optional.clone().map(|(field, optional)| {
+        let field_ident = field.ident.clone();
+        let field_ty_raw = field.ty.clone();
+        let field_ty = if !optional {quote! {Option<#field_ty_raw>}} else {quote! {#field_ty_raw}};
+        quote! {
+            #field_ident: #field_ty,
+        }
+    });
+    let builder_methods = fields_with_optional.clone().map(|(field, optional)| {
+        let field_ident = field.ident.clone();
+        let field_ty_raw = field.ty.clone();
+        let field_ty = if optional {quote! {Option<#field_ty_raw>}} else {quote! {#field_ty_raw}};
+        let field_set = if optional {quote! {#field_ident}} else {quote! {Some(#field_ident)}};
+        quote! {
+            fn #field_ident(&mut self, #field_ident: #field_ty) -> &mut Self {
+                self.#field_ident = #field_set;
+                self
+            }
+        }
+    });
+    let build_fields = fields_with_optional.clone().map(|(field, optional)| {
+        let field_ident = field.ident.clone();
+        let field_access = if optional {quote! {}} else {quote! {.ok_or("#field_ident field is missing")?}};
+        quote! {
+            #field_ident: self.#field_ident.clone()#field_access,
+        }
+    });
     quote! {
         impl #ident {
             pub fn builder() -> #builder {
@@ -17,34 +53,13 @@ pub fn derive(input: TokenStream) -> TokenStream {
         }
         #[derive(Eq, PartialEq, Default)]
         pub struct #builder {
-            executable: Option<String>,
-            args: Option<Vec<String>>,
-            env: Option<Vec<String>>,
-            current_dir: Option<String>,
+            #(#builder_fields)*
         }
         impl #builder {
-            fn executable(&mut self, executable: String) -> &mut Self {
-                self.executable = Some(executable);
-                self
-            }
-            fn args(&mut self, args: Vec<String>) -> &mut Self {
-                self.args = Some(args);
-                self
-            }
-            fn env(&mut self, env: Vec<String>) -> &mut Self {
-                self.env = Some(env);
-                self
-            }
-            fn current_dir(&mut self, current_dir: String) -> &mut Self {
-                self.current_dir = Some(current_dir);
-                self
-            }
+            #(#builder_methods)*
             pub fn build (&mut self) -> Result<#ident, Box<dyn std::error::Error>> {
                 Ok(#ident {
-                    executable: self.executable.clone().ok_or("executable field is missing")?,
-                    args: self.args.clone().ok_or("args field is missing")?,
-                    env: self.env.clone().ok_or("env field is missing")?,
-                    current_dir: self.current_dir.clone().ok_or("current_dir field is missing")?,
+                    #(#build_fields)*
                 })
             }
         }
