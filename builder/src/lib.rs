31441f5654b538c685a5f82274bb870e86663d3f
Hyunggyu Jang <murasakipurplez5@gmail.com>
2022-12-03 18:01:30 +0900

    Until 06-optional-fields

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,5 +1,5 @@
 use proc_macro::TokenStream;
-use quote::{quote, format_ident, ToTokens, __private::TokenTree};
+use quote::{quote, format_ident};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
@@ -10,37 +10,32 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let fields_with_optional = fields
         .iter()
         .map(|field| {
-            println!("Ty {:?}", field.ty);
-            let optional = if let Some(TokenTree::Ident(id)) = field.ty.to_token_stream().into_iter().next() {
-                id.to_string() == "Option"
-            } else {
-                false
-            };
-            (field, optional)
+            (field, extract_optioned_ty(&field.ty))
         });
     let builder_fields = fields_with_optional.clone().map(|(field, optional)| {
-        let field_ident = field.ident.clone();
-        let field_ty_raw = field.ty.clone();
-        let field_ty = if !optional {quote! {Option<#field_ty_raw>}} else {quote! {#field_ty_raw}};
+        let field_ident = &field.ident;
+        let field_ty_raw = &field.ty;
+        let field_ty = if optional.is_none() {quote! {Option<#field_ty_raw>}} else {quote! {#field_ty_raw}};
         quote! {
             #field_ident: #field_ty,
         }
     });
     let builder_methods = fields_with_optional.clone().map(|(field, optional)| {
-        let field_ident = field.ident.clone();
-        let field_ty_raw = field.ty.clone();
-        let field_ty = if optional {quote! {Option<#field_ty_raw>}} else {quote! {#field_ty_raw}};
-        let field_set = if optional {quote! {#field_ident}} else {quote! {Some(#field_ident)}};
+        let field_ident = &field.ident;
+        let field_ty = if let Some(extracted_ty) = optional {
+            extracted_ty
+        } else { &field.ty };
         quote! {
             fn #field_ident(&mut self, #field_ident: #field_ty) -> &mut Self {
-                self.#field_ident = #field_set;
+                self.#field_ident = Some(#field_ident);
                 self
             }
         }
     });
     let build_fields = fields_with_optional.clone().map(|(field, optional)| {
-        let field_ident = field.ident.clone();
-        let field_access = if optional {quote! {}} else {quote! {.ok_or("#field_ident field is missing")?}};
+        let field_ident = &field.ident;
+        let error_msg = format!("{} field is missing", field.ident.as_ref().unwrap().to_string());
+        let field_access = if optional.is_some() {quote! {}} else {quote! {.ok_or(#error_msg)?}};
         quote! {
             #field_ident: self.#field_ident.clone()#field_access,
         }
